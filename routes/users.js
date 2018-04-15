4713a92616bb711123a528f217c1386517d5476a
Nick Stellato <nick@nickstellato.com>
2018-04-14 22:00:03 -0500

    added user registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // Register
 router.post('/register', (req, res) => {
-    res.send('REGISTER');
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({success: false, msg:'Failed to register user'});
+        } else {
+            res.json({sucess: true, msg:'User registered'});
+        }
+    });
 });
 
 // Authenticate
